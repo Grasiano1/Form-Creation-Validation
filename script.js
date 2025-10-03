51b4fa16f40d205c914bf4aa2d1ded48ae9ccfba
Grasiano Dery <dewgrass77@gmail.com>
2025-10-03 22:19:57 +0000

    first commit of 3 files

---
+++ script.js
@@ -0,0 +1,5 @@
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const form = document.getElementById('registration-form');
+const feedbackDiv = document.getElementById('form-feedback');
